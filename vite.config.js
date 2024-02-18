539d32b70508fe40c428d0f06aea9b4a67ae71ef
Reinhart Previano Koentjoro <reinhart@reinhart1010.id>
2024-02-19 01:07:42 +0700

    try: fix leaf error

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite';
 import leaf from '@leafphp/vite-plugin';
 
-export default defineConfig({
-    plugins: [
+export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
+  if (command === 'serve') {
+    // Development mode
+    return {
+      plugins: [
         leaf({
-            input: ['app/views/js/app.js', 'app/views/css/app.css'],
-            refresh: true,
+          input: ['app/views/js/app.js', 'app/views/css/app.css'],
+          refresh: true,
         }),
-    ],
+      ],
+    }
+  } else {
+    // Production mode
+    return {
+      build: {
+        outDir: "./public/build/build",
+      },
+      plugins: [
+        leaf({
+          input: ['app/views/js/app.js', 'app/views/css/app.css'],
+          refresh: true,
+        }),
+      ],
+    }
+  }
 });
