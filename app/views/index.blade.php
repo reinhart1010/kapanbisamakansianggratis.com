e9042e21cf50b76bc3e83c250b4356ca089bc104
Reinhart Previano Koentjoro <reinhart@reinhart1010.id>
2024-02-19 01:35:57 +0700

    fix: production error

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -42,12 +42,12 @@
     <!-- Stylesheets -->
     <?php /* echo vite('css/app.css'); */ ?>
     {{-- <link rel="stylesheet" href="{{ assets('css/styles.css') }}"> --}}
-    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/styles.css">
     <link rel="stylesheet" href="https://reinhart1010.github.io/nacelle/nacelle.lite.min.css">
 
     <!-- Scripts -->
     {{-- <script src="{{ assets('js/countdown.min.js') }}"></script> --}}
-    <script src="/js/countdown.min.js"></script>
+    <script src="/assets/js/countdown.min.js"></script>
     <script>
         /* Imperative HTML */
         var now = new Date();
